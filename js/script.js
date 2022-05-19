68bf5b582544274d31c6935e3203782e32004752
Henrik Juhl (hjemme) <henrik.juhl_dk@hotmail.com>
2022-05-19 22:22:35 +0200

    small change so user can't click button before delay of 2 secs has occured

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const ADVICEGEN_API_URL = "https://api.adviceslip.com/advice";
+const MIN_DELAY =
+  2 * 1000; /* delay needed before next advice can be loaded in millisecs */
+
 const getData = async () => {
   const response = await fetch(ADVICEGEN_API_URL);
   const advice = await response.json();
@@ -6,12 +9,19 @@ const getData = async () => {
 };
 
 const updatePage = (advice) => {
+  const button = document.getElementById("loadAdviceBtn");
   const adviceNoValue = advice.id;
   const adviceTextValue = advice.advice;
   const adviceNoElement = document.getElementById("adviceId");
   const adviceTextElement = document.getElementById("adviceText");
   adviceNoElement.innerText = `ADVICE #${adviceNoValue}`;
   adviceTextElement.innerText = "“" + adviceTextValue + "”";
+  button.disabled = true;
+  button.classList.add("disabled");
+  setTimeout(() => {
+    button.disabled = false;
+    button.classList.remove("disabled");
+  }, MIN_DELAY);
 };
 
 getData();
